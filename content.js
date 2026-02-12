c6bf8f284b1abd7d180204aa53d8c4d48052c018
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-02-12 11:29:53 +0530

    feat: createdAIButton

--- content.js
+++ content.js
@@ -1,7 +1,61 @@
 console.log("Email writer extension content script loaded");
 
+function createdAIButton(){
+
+    const button = document.createElement('div');
+    button.className = 'T-I J-J5-Ji ao0 v7 T-I-atl L3';
+    button.style.marginRight = '8px';
+    button.innerHTML = 'AI Reply';
+    button.setAttribute('role', 'button');
+    button.setAttribute('data-tooltip', 'Generate AI Reply');
+    return button;
+
+}
+
+function findComposeToolbar(){
+
+
+    const selectors = [
+        '.btC',
+        '.aDh',
+        '[role="toolbar"]', 
+        '.gU.Up'
+    ];
+
+    for (const selector of selectors) {
+        const toolbar = document.querySelector(selector);
+        if(toolbar){
+            return toolbar;
+
+        }
+        return null;
+        
+    }
+
+}
 
 function injectButton(){
+    const existingButton = document.querySelector('.ai-reply-buton');
+
+    if(existingButton) existingButton.remove();
+
+    const toolbar = findComposeToolbar();
+    if(!toolbar){
+        console.log("Toolbar not found");
+        return;
+    }
+    console.log("Toolbar found, creating AI button");
+    const button = createdAIButton();
+    button.classList.add('ai-reply-buton');
+
+    button.addEventListener('click', async () => {
+        
+
+
+    });
+
+    toolbar.insertBefore(button, toolbar.firstChild );
+
 
 }
 
